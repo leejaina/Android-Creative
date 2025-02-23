ffce4449b003f2ae5b9c25d10637031d6bb8b127
leejaina <leejaina@konkuk.ac.kr>
2025-02-23 22:28:20 +0900

    feat:학생증 틀을 위한 composable을 만들었다.

--- app/src/main/java/com/example/androidcreative/MainActivity.kt
+++ app/src/main/java/com/example/androidcreative/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androidcreative.ui.theme.AndroidCreativeTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,5 +51,23 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     AndroidCreativeTheme {
         Greeting("Android")
+    }
+}
+
+@Preview
+@Composable
+fun StudentCard() {
+    Box( //학생증 틀
+        modifier = Modifier
+            .padding()
+            .height(80.dp)
+            .width(160.dp)
+            .clip(RoundedCornerShape(10.dp))
+    ) {
+        Image(
+            painter = painterResource(id=R.drawable.studentcard),
+            contentDescription = "학생증 틀",
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
