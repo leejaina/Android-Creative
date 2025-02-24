9f1c3a35c0101af8b2b60c8d12a66f7edeb27237
leejaina <leejaina@konkuk.ac.kr>
2025-02-24 23:45:54 +0900

    feat:파일 정리 리팩터링

--- app/src/main/java/com/example/androidcreative/MainActivity.kt
+++ app/src/main/java/com/example/androidcreative/MainActivity.kt
@@ -35,104 +35,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.androidcreative.ui.theme.AndroidCreativeTheme
+import profilecard.ProfileCardScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ProfileCardScreen()
-        }
-    }
-}
-
-
-@Preview
-@Composable //하나의 학생증을 만드는 컴포저블
-fun ProfileCardComponent() {
-    Box( //학생증 틀
-        modifier = Modifier
-            .height(80.dp)
-            .width(160.dp)
-            .clip(RoundedCornerShape(10.dp))
-    ) {
-        Image( //학생증 배경
-            painter = painterResource(id=R.drawable.studentcard),
-            contentDescription = "학생증 틀",
-            modifier = Modifier.fillMaxSize()
-        )
-
-        Box( // 프로필 내용 & 프사 틀
-            modifier = Modifier
-                .padding(horizontal = 10.dp, vertical = 10.dp)
-                .height(43.dp)
-                .width(116.dp),
-            contentAlignment = Alignment.CenterStart // 내부 요소 왼쪽 정렬
-        ) {
-            Row ( // 프사와 텍스트 가로 정렬
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Box( // 프사 틀
-                    modifier = Modifier
-                        .size(40.dp)
-                        .clip(RoundedCornerShape(10.dp))
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.profilepic),
-                        contentDescription = "프사",
-                        modifier = Modifier.fillMaxSize()
-                    )
-                }
-
-                Spacer(modifier = Modifier.width(10.dp)) // 프사와 텍 사이 여백
-
-                Column( // 프로필 정보
-                    verticalArrangement = Arrangement.spacedBy(5.dp)
-                ) {
-                    Text(text = "이름 : 이재인", fontSize = 8.sp)
-                    Text(text = "학번 : 202410801", fontSize = 8.sp)
-                    Text(text = "학과 : 기로자공학부", fontSize = 8.sp)
-                }
-            }
-        }
-
-        Box(
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(horizontal = 11.dp, vertical = 8.dp)
-        ) {
-            ProfileCardButton()
-        }
-    }
-}
-
-@Preview
-@Composable
-fun ProfileCardButton ( //버튼 기능을 위한 컴포저블
-    modifier: Modifier=Modifier
-) {
-    var isClicked by rememberSaveable { mutableStateOf(false) }
-
-    Image(
-        painter = painterResource(id=if (isClicked) R.drawable.blackbotton
-        else R.drawable.redbotton),
-        contentDescription = "버튼",
-        modifier=modifier
-            .size(15.dp)
-            .clickable { isClicked=!isClicked }
-    )
-}
-
-@Preview
-@Composable //전체 화면 구성. mainactivity와 이어진다.
-fun ProfileCardScreen() {
-    LazyColumn (
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(15.dp),
-        contentPadding = PaddingValues(top=11.dp, start = 13.dp)
-    ) {
-        items(7) {
-            ProfileCardComponent()
         }
     }
 }
