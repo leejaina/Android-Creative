4a1673b51541f37df26a6703176c7cc82e4014cf
leejaina <leejaina@konkuk.ac.kr>
2025-03-03 13:28:54 +0900

    feat:숫자 동그라미 컴포저블 맨듬

--- app/src/main/java/adddelete/AddDeleteScreen.kt
+++ app/src/main/java/adddelete/AddDeleteScreen.kt
@@ -1,4 +1,34 @@
 package adddelete
 
-class AddDeleteScreen {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview
+@Composable
+fun NumberCircle(number: Int=1) { //와 미친~ 프리뷰쓰려면 변수의 기본값을 정해야한다고??
+    Box(
+        modifier = Modifier
+            .size(80.dp)
+            .clip(CircleShape)
+            .background(Color(0xFFFAB398)),
+        contentAlignment = Center
+    ) {
+        Text(
+            text = number.toString(),
+            fontSize = 20.sp,
+            color = Color.White
+        )
+    }
 }
